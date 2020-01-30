ee17baedc22f1a7e1b2a33252edb5555dfdee3d0
rohitbhakaregit <bhakarerohit@gmail.com>
2020-01-31 01:49:06 +0530

    changes the few thing

--- fire.sh
+++ fire.sh
@@ -1,3 +1,3 @@
 echo "hi there I am Rohit" >>/tmp/rohit.txt
 echo $tellme >> /tmp/rohit.txt
-echo "finally I have achived that" >> /tmp/rohit.txt
+echo "finally I have achived hat" >> /tmp/rohit.txt
