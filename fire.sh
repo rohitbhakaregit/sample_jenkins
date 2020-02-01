a421defb70d23fe6c4c99eb0450713a7e8a59e37
rohitbhakaregit <bhakarerohit@gmail.com>
2020-02-01 21:52:48 +0530

    updated the definaltion

--- fire.sh
+++ fire.sh
@@ -1,3 +1,3 @@
 echo "hi there I am Rohit" >>/tmp/rohit.txt
 echo $tellme >> /tmp/rohit.txt
-echo "finally I have achived the jenkins webhook" >> /tmp/rohit.txt
+echo "finally I have achived the jenkins webhoo11111111111k" >> /tmp/rohit.txt
